7cf34a7ece0cb9a9b0cc55ce742e6bd1739882b4
Graham Swan <thinkswan@gmail.com>
2019-08-22 15:26:12 +0200

    Catch public repo count in Redis

--- index.js
+++ index.js
@@ -8,16 +8,26 @@ const REDIS_PORT = process.env.redis_port || 6379
 const client = redis.createClient(REDIS_PORT)
 const app = express()
 
+function setResponse(username, repos) {
+  return `<h2>${username} has ${repos} public repos on GitHub<h2>`
+}
+
 async function getRepos(req, res, next) {
   try {
     const { username } = req.params
 
-    console.log(`Fetching data for ${username}`)
+    console.log(`Fetching GitHub data for ${username}...`)
 
     const response = await fetch(`https://api.github.com/users/${username}`)
     const data = await response.json()
+    const repos = data.public_repos
 
-    res.send(data)
+    console.log(`Found ${repos} public repos for ${username}`)
+
+    // Cache data in Redis
+    client.setex(username, 3600, repos)
+
+    res.send(setResponse(username, repos))
   } catch (error) {
     console.error(error)
 
