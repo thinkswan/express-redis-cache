5db9ea56503c5d44d194ab22432e990ac5528cef
Graham Swan <thinkswan@gmail.com>
2019-08-22 15:23:04 +0200

    Add /repos/:username route to fetch GitHub repos

--- index.js
+++ index.js
@@ -8,6 +8,25 @@ const REDIS_PORT = process.env.redis_port || 6379
 const client = redis.createClient(REDIS_PORT)
 const app = express()
 
+async function getRepos(req, res, next) {
+  try {
+    const { username } = req.params
+
+    console.log(`Fetching data for ${username}`)
+
+    const response = await fetch(`https://api.github.com/users/${username}`)
+    const data = await response.json()
+
+    res.send(data)
+  } catch (error) {
+    console.error(error)
+
+    res.status(500)
+  }
+}
+
+app.get("/repos/:username", getRepos)
+
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`)
 })
